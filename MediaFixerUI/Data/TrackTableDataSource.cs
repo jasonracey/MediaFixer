b22fabbaca6d8bd2992d99da1c9dfa77c368851d
Jason Racey <jasonracey@gmail.com>
2022-04-04 19:01:10 -0700

    Enable column sorting

--- MediaFixerUI/Data/TrackTableDataSource.cs
+++ MediaFixerUI/Data/TrackTableDataSource.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using AppKit;
+using Foundation;
 
 namespace MediaFixerUI.Data
 {
@@ -11,6 +12,44 @@ namespace MediaFixerUI.Data
         public override nint GetRowCount(NSTableView tableView)
         {
             return Tracks.Count;
+        }
+
+        public override void SortDescriptorsChanged (NSTableView tableView, NSSortDescriptor[] oldDescriptors)
+        {
+            if (oldDescriptors.Length > 0) 
+            {
+                Sort(oldDescriptors [0].Key, oldDescriptors [0].Ascending);
+            } 
+            else
+            {
+                var sortDescriptors = tableView.SortDescriptors; 
+                Sort(sortDescriptors[0].Key, sortDescriptors[0].Ascending); 
+            }
+            tableView.ReloadData();
+        }
+        
+        private void Sort(string key, bool asc) 
+        {
+            switch (key) 
+            {
+                case "Track #":
+                    Tracks.Sort((x, y) => GetOrder(asc) * x.Number.CompareTo(y.Number));
+                    break;
+                case "Title":
+                    Tracks.Sort((x, y) => GetOrder(asc) * string.Compare(x.Title, y.Title, StringComparison.Ordinal));
+                    break;
+                case "Album":
+                    Tracks.Sort((x, y) => GetOrder(asc) * string.Compare(x.Album, y.Album, StringComparison.Ordinal));
+                    break;
+                case "Disc #":
+                    Tracks.Sort((x, y) => GetOrder(asc) * x.DiscNumber.CompareTo(y.DiscNumber));
+                    break;
+            }
+        }
+        
+        private static int GetOrder(bool asc)
+        {
+            return asc ? 1 : -1;
         }
     }
 }
