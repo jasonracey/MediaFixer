6e2e1b11e856c1b75bcc9e854acb8ff5675b44b0
Jason Racey <jasonracey@gmail.com>
2022-04-16 07:40:55 -0700

    Remove deprecated project from deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# deploy console
-echo "Deploying Console..."
-rm -Rf ../../Tools/MediaFixer
-mkdir ../../Tools/MediaFixer
-cp -R ./MediaFixerConsole/bin/Release/netstandard2.1/ ../../Tools/MediaFixer
-
 # deploy app
 echo "Deploying UI..."
 rm -Rf /Applications/Media\ Fixer.app
