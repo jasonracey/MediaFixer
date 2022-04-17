05c8f063b75f01b16cb65344520e8e264c9053fe
Jason Racey <jasonracey@gmail.com>
2022-04-16 21:32:33 -0700

    Add unit test project and first tests

---
+++ MediaFixerLib.UnitTests/MediaFixerStatusTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace MediaFixerLib.UnitTests
+{
+    [TestFixture]
+    public class StatusTests
+    {
+        [Test]
+        public void ItemsTotal_MustBeGreaterThanOrEqualToZero()
+        {
+            // arrange
+            const int itemsTotal = -1;
+
+            // act/assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => MediaFixerStatus.Create(itemsTotal));
+        }
+
+        [Test]
+        public void Create_WithoutMessage()
+        {
+            // arrange
+            const int itemsTotal = 1;
+
+            // act
+            var status = MediaFixerStatus.Create(itemsTotal);
+
+            // assert
+            status.ItemsProcessed.Should().Be(0);
+            status.ItemsTotal.Should().Be(itemsTotal);
+            status.Message.Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void Create_WithMessage()
+        {
+            // arrange
+            const int itemsTotal = 1;
+            var message = Guid.NewGuid().ToString();
+
+            // act
+            var status = MediaFixerStatus.Create(itemsTotal, message);
+
+            // assert
+            status.ItemsProcessed.Should().Be(0);
+            status.ItemsTotal.Should().Be(itemsTotal);
+            status.Message.Should().Be(message);
+        }
+
+        [Test]
+        public void ItemsProcessed_CanIncrement()
+        {
+            // arrange
+            const int iterations = 3;
+            var status = MediaFixerStatus.Create(default);
+
+            // act
+            for (var i = 0; i < iterations; i++) status.ItemProcessed();
+
+            // assert
+            status.ItemsProcessed.Should().Be(iterations);
+        }
+    }
+}
